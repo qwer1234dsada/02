bbf77f1cc39b5c5f6dc64727189985ba9448d8d7
qwer1234dsada <yungjun0319@gmail.com>
2021-09-09 20:37:01 +0900

    Create grid.py

---
+++ grid.py
@@ -0,0 +1,21 @@
+import turtle
+count = 0
+while ( count < 5):
+    turtle.forward(500)
+    turtle.right(180)
+    turtle.forward(500)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    count = count+1
+turtle.forward(500)
+turtle.left(90)
+count = 0
+while ( count < 5):
+    turtle.forward(500)
+    turtle.right(180)
+    turtle.forward(500)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    count = count+1
